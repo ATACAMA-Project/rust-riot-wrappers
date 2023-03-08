e578f3ea9f2850f01e4f1fab00b6fe81f7eacc24
denis <dbraunag@uni-bremen.de>
2023-03-08 18:22:10 +0100

    Rest hinzufuegen

--- src/nanocoap.rs
+++ src/nanocoap.rs
@@ -5,7 +5,18 @@ use core::ffi::{c_char, c_int, c_void};
 
 
 
-//! Relevant structs from nanocoap.h
+//! Relevant structs and enums from nanocoap.h
+//todo: add remaining structs and enums
+
+pub enum CoapMethodFlags {
+    GET = riot_sys::COAP_METHOD_GET as isize,
+    PUT = riot_sys::COAP_METHOD_PUT as isize,
+    POST = riot_sys::COAP_METHOD_POST as isize,
+    DELETE = riot_sys::COAP_METHOD_DELETE as isize,
+    FETCH = riot_sys::COAP_METHOD_FETCH as isize,
+    PATCH = riot_sys::COAP_METHOD_PATCH as isize,
+    IPATCH = riot_sys::COAP_METHOD_IPATCH as isize,
+}
 
 pub struct CoapResource {
     resource: *mut riot_sys::coap_resource_t
@@ -22,13 +33,12 @@ pub struct CoapRequestContext {
 
 //! Defining relevant methods for coap_resource_t
 impl CoapResource {
-    fn new(path: &str, flags: CoapMethodFlagT, handler: CoapHandler, ctx: CoapRequestContext) -> CoapResource {
-        let _path = path.as_bytes().to_cstr().unwrap().as_ptr();
-        let _flags: riot_sys::coap_method_flags_t;
-        let _handler: riot_sys::coap_handler_t;
-        let _context: *mut c_void;
-        
-        return CoapResource {
+    //todo: add remaining methods
+
+    //! Beispiel, wie eine Methode aussehen kann
+    //todo: fix pointer issue
+    fn new(path: &str, flags: CoapMethodFlags, handler: CoapHandler, ctx: CoapRequestContext) -> CoapResource {
+             CoapResource {
             resource: riot_sys::coap_resource_t {
                 path: path.as_bytes().to_cstr().unwrap().as_ptr(),
                 methods: flags as riot_sys::coap_method_flags_t,
