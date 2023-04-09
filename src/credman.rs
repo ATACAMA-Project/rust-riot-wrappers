0ef53a8b30be070b5646953aa7e7a68374602c2d
Lukas Terlau <terlau@uni-bremen.de>
2023-04-09 15:30:13 +0200

    credman Vorschläge von Joel umgesetzt.

--- src/credman.rs
+++ src/credman.rs
@@ -4,106 +4,9 @@
 
 
 
-
+use core::ptr;
 use riot_sys::libc::c_void;
 use riot_sys::size_t;
-
-pub struct CredmanCredential{
-    cred: riot_sys::credman_credential_t,
-}
-
-pub struct CredmanBuffer{
-    cred_buf: riot_sys::credman_buffer_t,
-}
-
-pub struct CredmanPskParams {
-    psk: riot_sys::psk_params_t,
-}
-
-pub struct CredmanEcdsaPublicKey {
-    ecdsa_public_key: riot_sys::ecdsa_public_key_t,
-}
-
-pub struct CredmanEcdsaParams {
-    ecdsa: riot_sys::ecdsa_params_t ,
-}
-
-type CredmanTag = u16;
-
-
-impl CredmanBuffer {
-    pub fn new (s: *const c_void, len: usize) -> Self{
-        CredmanBuffer {
-            cred_buf: riot_sys::credman_buffer_t{
-                s:s,
-                len:len as size_t,
-            }
-        }
-    }
-}
-
-impl CredmanPskParams {
-    pub fn new (key: &CredmanBuffer, id: &CredmanBuffer, hint: &CredmanBuffer) -> Self {
-        CredmanPskParams{
-            psk: riot_sys::psk_params_t{
-                key: key.cred_buf,
-                id: id.cred_buf,
-                hint: hint.cred_buf,
-            }
-        }
-    }
-}
-
-impl CredmanEcdsaPublicKey {
-    pub fn new ( x: *const c_void, y: *const c_void) -> Self {
-        CredmanEcdsaPublicKey{
-            ecdsa_public_key: riot_sys::ecdsa_public_key_t{
-                x:x,
-                y:y,
-            }
-        }
-    }
-}
-
-impl CredmanEcdsaParams {
-    pub fn new (private_key: *const c_void, public_key: &CredmanEcdsaPublicKey, client_key: &mut CredmanEcdsaPublicKey, client_keys_size: usize) -> Self {
-        CredmanEcdsaParams{
-            ecdsa: riot_sys::ecdsa_params_t {
-                private_key: private_key,
-                public_key: public_key.ecdsa_public_key,
-                client_keys: &mut client_key.ecdsa_public_key as *mut riot_sys::ecdsa_public_key_t,
-                client_keys_size: client_keys_size as size_t,
-            }
-        }
-    }
-}
-
-
-impl CredmanCredential{
-    pub fn new_psk ( tag: CredmanTag, psk: &CredmanPskParams) -> Self {
-        CredmanCredential { 
-            cred: riot_sys::credman_credential_t{
-                type_:riot_sys::credman_type_t_CREDMAN_TYPE_PSK,
-                tag: tag,
-                params: riot_sys::credman_credential_t__bindgen_ty_1 {psk: psk.psk,},
-            }
-        }
-    }
-    pub fn new_ecdsa ( tag: CredmanTag, ecdsa: &CredmanEcdsaParams) -> Self {
-        CredmanCredential { 
-            cred: riot_sys::credman_credential_t{
-                type_:riot_sys::credman_type_t_CREDMAN_TYPE_ECDSA,
-                tag: tag,
-                params: riot_sys::credman_credential_t__bindgen_ty_1 {ecdsa: ecdsa.ecdsa,},
-            }
-        }
-
-    }
-}
-
-
-
-
 
 #[derive(Debug)]
 #[non_exhaustive]
@@ -131,9 +34,7 @@ impl CredmanStatus {
             -6 => Self::CredmanError,
             _ => Self::CredmanStatusUnknown(n),
         }
-    }
-
-    
+    } 
 }
 
 
@@ -146,17 +47,8 @@ pub enum CredmanType {
 }
 
 impl CredmanType {
-    /// Converts the given `c_int` into the matching Enum representation
-    fn from_value(n: riot_sys::credman_type_t) -> Self {
-        match n {
-            riot_sys::credman_type_t_CREDMAN_TYPE_EMPTY => Self::CredmanTypeEmpty,
-            riot_sys::credman_type_t_CREDMAN_TYPE_PSK => Self::CredmanTypePSK,
-            riot_sys::credman_type_t_CREDMAN_TYPE_ECDSA =>  Self::CredmanTypeECDSA,
-            _ => Self::CredmanTypeUnknown(n),
-        }
-    }
 
-    fn to_value(n: CredmanType) -> riot_sys::credman_type_t{
+    fn to_c (n: CredmanType) -> riot_sys::credman_type_t{
         match n {
             Self::CredmanTypeEmpty => riot_sys::credman_type_t_CREDMAN_TYPE_EMPTY,
             Self::CredmanTypePSK => riot_sys::credman_type_t_CREDMAN_TYPE_PSK,
@@ -166,28 +58,160 @@ impl CredmanType {
     }
 }
 
+
+
+pub struct CredentialRef{
+    pub credential: riot_sys::credman_credential_t,
+}
+
+type CredmanTag = u16;
+
+pub enum Params <'a>{
+    Psk(PskParams<'a>),
+    Ecdsa(EcdsaParams<'a>),
+}
+
+
+pub struct PskParams <'a>{
+    pub key: &'a[u8],
+    pub id: &'a[u8],
+    pub hint: &'a[u8],
+}
+
+pub struct EcdsaPublicKey<'a> {
+    pub x: &'a[u8],
+    pub y: &'a[u8],
+}
+
+pub struct EcdsaParams<'a> {
+    pub private_key: &'a[u8],
+    pub public_key: EcdsaPublicKey<'a>,
+    client_keys: &'a [riot_sys::ecdsa_public_key_t],
+}
+
+
+
+impl <'a> EcdsaParams <'a> {
+    pub fn new <const EC_CLIENT_KEYS_NUM: usize> (private_key: &'a[u8], public_key: EcdsaPublicKey<'a>, client_keys: &'a EcdsaClientKeys<EC_CLIENT_KEYS_NUM>) -> Self{
+        EcdsaParams{
+            private_key,
+            public_key,
+            client_keys: &client_keys.client_keys_c,
+        }
+    }
+}
+
+
+pub struct EcdsaClientKeys< const EC_CLIENT_KEYS_NUM: usize>{
+    client_keys_c: [riot_sys::ecdsa_public_key_t;EC_CLIENT_KEYS_NUM],
+}
+
+impl < const EC_CLIENT_KEYS_NUM: usize> EcdsaClientKeys< EC_CLIENT_KEYS_NUM> {
+    fn new (client_keys: &[EcdsaPublicKey]) -> Self{
+        let mut keys = EcdsaClientKeys{
+            client_keys_c:[riot_sys::ecdsa_public_key_t{x: ptr::null(), y: ptr::null(),};EC_CLIENT_KEYS_NUM],
+        };
+        let mut i=0;
+        for key in client_keys{
+            keys.client_keys_c[i]=riot_sys::ecdsa_public_key_t{
+                x: key.x.as_ptr() as *const c_void,
+                y: key.y.as_ptr() as *const c_void,
+            };
+            i+=1;
+        }
+        keys
+    }
+
+    
+}
+
+
+pub struct Credential<'a> {
+    credential: riot_sys::credman_credential_t,
+    params: Params<'a> ,
+}
+
+impl<'a> Credential <'a>{
+    
+    pub fn new_psk ( tag: CredmanTag, psk: PskParams<'a>) -> Self {
+        let psk_c= riot_sys::psk_params_t{
+            key : riot_sys::credman_buffer_t{
+                s: psk.key.as_ptr() as *const c_void,
+                len: psk.key.len() as size_t,
+            },
+            id : riot_sys::credman_buffer_t{
+                s: psk.id.as_ptr() as *const c_void,
+                len: psk.id.len() as size_t,
+            },
+            hint : riot_sys::credman_buffer_t{
+                s: psk.hint.as_ptr() as *const c_void,
+                len: psk.hint.len() as size_t,
+            },
+        };
+        Credential { 
+            credential: riot_sys::credman_credential_t{
+                type_:riot_sys::credman_type_t_CREDMAN_TYPE_PSK,
+                tag: tag,
+                params: riot_sys::credman_credential_t__bindgen_ty_1 {psk: psk_c},
+            },
+            params: Params::Psk(psk),
+        }
+    }
+
+    pub fn new_ecdsa ( tag: CredmanTag, ecdsa: EcdsaParams<'a>) -> Self {
+        let ecdsa_c= riot_sys::ecdsa_params_t{
+            private_key: ecdsa.private_key.as_ptr() as *const c_void,
+            public_key: riot_sys::ecdsa_public_key_t{
+                x: ecdsa.public_key.x.as_ptr() as *const c_void,
+                y: ecdsa.public_key.y.as_ptr() as *const c_void,
+            },
+            client_keys: ecdsa.client_keys.as_ptr() as *mut _, //Is assumed not to be mutated during the runtime? 
+            client_keys_size: ecdsa.client_keys.len() as size_t,
+        };
+
+        Credential { 
+            credential: riot_sys::credman_credential_t{
+                type_:riot_sys::credman_type_t_CREDMAN_TYPE_ECDSA,
+                tag: tag,
+                params: riot_sys::credman_credential_t__bindgen_ty_1 {ecdsa:ecdsa_c},
+            },
+            params: Params::Ecdsa(ecdsa),
+        }
+
+    }
+}
+
+
+pub fn scope < Main, RMain> (credential: &Credential, main: Main) -> Result<RMain,CredmanStatus>
+where
+Main: FnOnce() -> RMain,
+{
+    let status=credman_add(credential);
+    if matches!(status,CredmanStatus::CredmanOK) {
+        return Err(status)
+    }
+    let ret= main();
+    unsafe{ riot_sys::credman_delete(credential.credential.tag,credential.credential.type_);}
+    Ok(ret)
+}
+
+
 // int credman_add(const credman_credential_t *credential);
-pub fn credman_add(cred: &CredmanCredential) -> CredmanStatus{
-    let res = unsafe {riot_sys::credman_add(&cred.cred as *const riot_sys::credman_credential_t)};
+fn credman_add(credential: &Credential) -> CredmanStatus{
+    let res = unsafe {riot_sys::credman_add(&credential.credential as *const riot_sys::credman_credential_t)};
     CredmanStatus::from_value(res)
 }
 
 // int credman_get(credman_credential_t *credential, credman_tag_t tag, credman_type_t type);
-pub fn credman_get( tag: CredmanTag, typ: CredmanType) -> Result<CredmanCredential,CredmanStatus>{
+pub fn credman_get( tag: CredmanTag, typ: CredmanType) -> Result<CredentialRef,CredmanStatus>{ //TOD: CredmanCredRef zurückgeben
     let mut cred : riot_sys::credman_credential_t = Default::default();
-    let res = unsafe{riot_sys::credman_get(&mut cred as *mut riot_sys::credman_credential_t,tag,CredmanType::to_value(typ))};
+    let res = unsafe{riot_sys::credman_get(&mut cred as *mut riot_sys::credman_credential_t,tag,CredmanType::to_c (typ))};
     match CredmanStatus::from_value(res) {
-        CredmanStatus::CredmanOK => Ok(CredmanCredential { cred: cred }),
+        CredmanStatus::CredmanOK => Ok(CredentialRef { credential: cred }),
         status => Err(status),
     }
 }
 
-// void credman_delete(credman_tag_t tag, credman_type_t type);
-pub fn credman_delete(tag: CredmanTag, typ: CredmanType) {
-    unsafe {
-        riot_sys::credman_delete(tag,CredmanType::to_value(typ))
-    };
-}
 
 // int credman_get_used_count(void);
 pub fn credman_get_used_count() -> u32{
@@ -195,50 +219,3 @@ pub fn credman_get_used_count() -> u32{
 }
 
 
-/* 
-// int credman_load_public_key(const void *buf, size_t buf_len, ecdsa_public_key_t *out);
-#[cfg(riot_module_credman_load)]
-pub fn credman_load_public_key( buf : *const c_void , mut buf_len : u32, out : *mut riot_sys::ecdsa_public_key_t) -> Result<riot_sys::ecdsa_public_key_t,CredmanStatus>{
-    unsafe {
-        match CredmanStatus::from_value(riot_sys::credman_load_public_key(buf,buf_len,out)) {
-            CredmanStatus::CredmanOK => Ok(*out),
-            status => Err(status),
-        }
-    }
-}
-
-// int credman_load_private_key(const void *buf, size_t buf_len, credman_credential_t *cred);
-#[cfg(riot_module_credman_load)]
-pub fn credman_load_private_key( buf : *const c_void , mut buf_len : u32) -> Result<CredmanCredential,CredmanStatus>{
-    let mut cred : riot_sys::credman_credential_t = Default::default();
-    unsafe {
-        match CredmanStatus::from_value(riot_sys::credman_load_private_key(buf,buf_len,&mut cred as *mut riot_sys::credman_credential_t)) {
-            CredmanStatus::CredmanOK => Ok(CredmanCredential { cred: cred }),
-            status => Err(status),  
-        }
-    }
-}
-
-// int credman_load_private_ecc_key(const void *buf, size_t buf_len, credman_credential_t *cred);
-#[cfg(riot_module_credman_load)]
-pub fn credman_load_private_ecc_key( buf : *const c_void , mut buf_len : u32) -> Result<CredmanCredential,CredmanStatus>{
-    let mut cred : riot_sys::credman_credential_t = Default::default();
-    unsafe {
-        match CredmanStatus::from_value(riot_sys::credman_load_private_ecc_key(buf,buf_len,&mut cred as *mut riot_sys::credman_credential_t)) {
-            CredmanStatus::CredmanOK => Ok(CredmanCredential { cred: cred }),
-            status => Err(status),  
-        }
-    }
-}
-// #endif here
-
-// #if here
-// void credman_reset(void);
-/*pub fn credman_reset() {
-    unsafe {
-        riot_sys::credman_reset();
-    }
-}*/
-// #endif here
-
-*/
